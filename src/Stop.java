a056ffd3c75687e76a609fac8b427dac4d6066c3
Abigail Amelia Amethyst <amethysa@tcd.ie>
2022-03-31 11:29:14 +0100

    Removed Transfer.java and created Stop.java

---
+++ src/Stop.java
@@ -0,0 +1,22 @@
+public class Stop {
+    int stop_id;
+    String stop_name;
+
+    Stop(int stop_id, String stop_name) {
+        this.stop_id = stop_id;
+
+        if (stop_name.indexOf("FLAGSTOP") != -1 || stop_name.indexOf("WB") != -1 || stop_name.indexOf("NB") != -1 ||
+                stop_name.indexOf("SB") != -1 || stop_name.indexOf("EB") != -1) {
+            String[] split = stop_name.split("\\s+", 2);
+            this.stop_name = split[1] + " " + split[0];
+        }
+    }
+
+    int getStop_id() {
+        return stop_id;
+    }
+
+    String getStop_name() {
+        return stop_name;
+    }
+}
