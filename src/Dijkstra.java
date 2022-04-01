7fb7f547e4b82f7111df4ae83367c3f45ddc4998
Abigail Amelia Amethyst <amethysa@tcd.ie>
2022-04-01 16:24:55 +0100

    Started Dijkstra algorithm (unfinished)

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,8 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dijkstra {
     String[] shortestPath;
     double minimumCost;
 
     Dijkstra(Graph graph, int src, int dst) {
+        int vertices = graph.stopMap.size();
+        HashMap<Integer, Double> shortestDists = new HashMap<>();
+        HashMap<Integer, Boolean> visited = new HashMap<>();
+
+        for (Map.Entry<Integer, Stop> i : graph.stopMap.entrySet()) {
+            shortestDists.put(i.getKey(), Double.POSITIVE_INFINITY);
+            visited.put(i.getKey(), false);
+        }
+
+        shortestDists.put(src, 0.0);
 
     }
 }
