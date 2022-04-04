67e1193516c8224853f5441c5433b9ef6759f0a1
Abigail Amelia Amethyst <amethysa@tcd.ie>
2022-04-04 16:32:37 +0100

    Developed Dijkstra algorithm (will probably need to overhaul this and graph.java because it does not work as intended)

--- src/BusSystem.java
+++ src/BusSystem.java
@@ -5,5 +5,10 @@
 public class BusSystem {
     public static void main(String[] args) {
         String[] inputs = {"stop_times.txt", "stops.txt", "transfers.txt"};
+        Graph test = new Graph(inputs);
+        Dijkstra dij = new Dijkstra(test, 646, 1281);
+
+        System.out.println("Minimum Cost is:" + dij.minimumCost);
+        System.out.println("List of stops en route:\n" + dij.shortestPath);
     }
 }
--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,21 +1,87 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Dijkstra {
-    String[] shortestPath;
+    ArrayList<Edge> shortestPath;
     double minimumCost;
 
     Dijkstra(Graph graph, int src, int dst) {
-        int vertices = graph.stopMap.size();
         HashMap<Integer, Double> shortestDists = new HashMap<>();
         HashMap<Integer, Boolean> visited = new HashMap<>();
+        HashMap<Integer, Edge> edgeTo = new HashMap<>();
 
-        for (Map.Entry<Integer, Stop> i : graph.stopMap.entrySet()) {
-            shortestDists.put(i.getKey(), Double.POSITIVE_INFINITY);
-            visited.put(i.getKey(), false);
+        for (int i : graph.stopMap.keySet()) {
+            shortestDists.put(i, Double.POSITIVE_INFINITY);
+            visited.put(i, false);
+            edgeTo.put(i, null);
         }
 
         shortestDists.put(src, 0.0);
 
+        boolean skipFirst = false;
+        Edge tmp = null;
+        for (int i : visited.keySet()) {
+            if (!skipFirst) {
+                skipFirst = true;
+                continue;
+            }
+            int nearest = minDist(shortestDists, visited);
+            visited.put(nearest, true);
+
+            double minWeight = Double.MAX_VALUE;
+            for (int j = 0; j < graph.stopMap.get(nearest).edges.size(); j++) {
+                if (graph.stopMap.get(nearest).edges.get(j).getWeight() < minWeight) {
+                    minWeight = graph.stopMap.get(nearest).edges.get(j).getWeight();
+                    tmp = graph.stopMap.get(nearest).edges.get(j);
+                }
+            }
+            assert tmp != null;
+            shortestDists.put(tmp.getStopBID(), minWeight + shortestDists.get(nearest));
+            edgeTo.put(nearest, tmp);
+            if (tmp.stopB.getStop_id() == dst) break;
+        }
+        minimumCost = shortestDists.get(dst);
+        shortestPath = getShortest(src, dst, edgeTo);
+    }
+
+    int minDist(HashMap<Integer, Double> shorts, HashMap<Integer, Boolean> vis) {
+        double min = Double.MAX_VALUE;
+        int min_id = -1;
+
+        for (int i : vis.keySet()) {
+            if (!vis.get(i) && shorts.get(i) <= min) {
+                min = shorts.get(i);
+                min_id = i;
+            }
+        }
+        return min_id;
+    }
+
+    ArrayList<Edge> getShortest(int src, int dst, HashMap<Integer, Edge> edgeTo) {
+        ArrayList<Edge> output = new ArrayList<>();
+        boolean srcFound = false;
+        boolean noPathFound = false;
+
+        Edge tmp = edgeTo.get(dst);
+        while (!srcFound) {
+            output.add(tmp);
+            if (tmp.stopA.getStop_id() == src) {
+                srcFound = true;
+            } else {
+                if (tmp.stopA != null) {
+                    tmp = edgeTo.get(tmp.stopA.getStop_id());
+                } else {
+                    noPathFound = true;
+                    break;
+                }
+            }
+        }
+
+        if (!noPathFound) {
+            return output;
+        } else {
+            return null;
+        }
     }
 }
